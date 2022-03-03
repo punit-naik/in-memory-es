65f554edf5f817acb44eb2b00346e9a3c57ae0ae
Punit Naik <naik.punit44@gmail.com>
2022-03-04 02:02:51 +0530

    added group to repo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject in-memory-es "0.1.0"
+(defproject org.clojars.punit-naik/in-memory-es "0.1.0"
   :description "A Clojure library designed to work with an in-memory version of Elasticsearch for testing purposes."
   :url "https://github.com/punit-naik/in-memory-es"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
